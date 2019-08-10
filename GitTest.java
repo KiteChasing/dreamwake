fc58406bfaa4470c273ae51698270eb97b64310b
KiteChasing <953623009@qq.com>
2019-08-10 10:36:39 +0800

    GitTest.java

---
+++ GitTest.java
@@ -0,0 +1,12 @@
+package java.git;
+
+/**
+ * @Description \n
+ * Created by lizq at 2019/8/9 17:12
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("git demo");
+
+    }
+}
